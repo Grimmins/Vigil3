96e858737c866fdf412b3627cf12ce02745adafb
Grimmins <lukas.koltes@hotmail.fr>
2025-10-14 21:58:59 +0200

    - add examples : a.sol, human_printer.sol - add task for slither

--- src/tasks.ts
+++ src/tasks.ts
@@ -1,4 +1,5 @@
 import { task } from "hardhat/config";
+import { runSlither } from "./slither-runner";
 
 task("hello", "hello from plugin :)").setAction(async (_, hre) => {
   hre.LintKey.sayHello();
@@ -6,6 +7,7 @@ task("hello", "hello from plugin :)").setAction(async (_, hre) => {
 
 task("lintkey", "analysis of a Solidity file with Slither")
   .addPositionalParam("file", "path of solidity file to analyze")
-  .setAction(async () => {
+  .setAction(async (taskArgs) => {
+    const reportPath = runSlither(taskArgs.file);
   });
 
